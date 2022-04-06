d7223493e2e9acf85582a1eaac1a87675bd37c50
Minliahmetova Violetta <vettaify@gmail.com>
2022-04-06 21:47:19 +0600

    2

--- 1.java
+++ 1.java
@@ -11,11 +11,10 @@ public class Main {
 
 
 
-public class Main {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price);
-        System.out.println(miles);
+public class BonusMilesService {
+    public int calculate(int price) {
+        int oneMile = 20;
+        int miles = price / oneMile;
+        return miles;
     }
 }
